5b1a5df5057efcf76f2e0dd03cfde368e4c33fe3
ludai <ludai0106@gmail.com>
2016-04-30 22:53:25 +0200

    update sample function

--- Assignment1/src/ass1.m
+++ Assignment1/src/ass1.m
@@ -1,20 +1,19 @@
 figure
 hold all
 mark={[1,0.4,0.6],[0.8,0.2,0.8],[0.5,0.8,1],[0.5,0.3,0.9],[0.2,0.8,0.2]};
-sample_size = 10
+sample_size = 100
 x_tst = 1:0.1:30;
 h=zeros(1,6)
 for fit = 1:5
-    for i=1:200
-        [dataset,x_sample,y_sample] = sample_data_with_noise(sample_size)
-        %Linear regression, curve fitting using least square error
-        %get coefficients
+    for i=1:150
+       [dataset,x_sample,y_sample] = sample_data_with_noise(sample_size)
+%perform regression, curve fitting using least square error
+%get coefficients
         p = polyfit(x_sample,y_sample,fit)
-        %test with bigger range   
+%test with bigger range   
         y_tst = polyval(p,x_tst);
         %plot(x_sample,y_sample,'o',x_tst,y_tst)
-        h(fit)=plot(x_tst,y_tst,'Color',mark{fit})
-
+        h(fit) = plot(x_tst,y_tst,'Color',mark{fit})
     end
 end
 h(6)=plot(x_tst,fun(x_tst),'-k','LineWidth',2)  
--- Assignment1/src/sample_data_with_noise.m
+++ Assignment1/src/sample_data_with_noise.m
@@ -1,21 +1,15 @@
 function [dataset,x_sample,y_sample] = sample_data_with_noise(sample_size)
 %Generate dataset
-    x = 0:1:10
+    x = 0:0.01:10
+%add gaussian noise
     mu = fun(x)
     [m,n]=size(mu)
     sigma = ones(1,n)/1000
-    num = 1
     %plot(x,mu)
     %grid on
-    dataset = mvnrnd(mu, sigma, num)
-    %random sample
-    index = sort(randsample(n,sample_size))
-    s_i = size(index)
-    x_sample = zeros(s_i(1,1),1)
-    y_sample = zeros(s_i(1,1),1)
-    for i=1:s_i(1,1)
-        x_sample(i,1) = x(1,index(i,1))
-        y_sample(i,1) = dataset(1,index(i,1))
-    end
+    dataset = mvnrnd(mu, sigma, 1)
+%random sample
+    [x_sample,idx] = datasample(x,sample_size,'Replace',false)
+    y_sample = dataset(idx);
     %scatter(x_sample,y_sample)
 end
